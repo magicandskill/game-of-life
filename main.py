bcdf0cfa0e0d5353289dd415ccf210928aca6c49
Andy <andy@Andys-MacBook-Pro.local>
2023-12-13 15:51:10 -0500

    game loop

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import pygame
-from state import get_initial_state, compute_new_state
+from state import get_initial_state
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
-clock = pygame.time.Clock()
 is_running = True
+clock = pygame.time.Clock()
 
 with open('config.txt', 'r') as f:
   state = get_initial_state(f.readlines())
-
-def draw_grid(grid):
-  for i, row in enumerate(grid):
-    for j, cell in enumerate(row):
-      draw_cell(cell, i, j)
-
-def draw_cell(cell, i, j):
-  leftTop = (j * 10, i * 10)
-  widthHeight = (10, 10)
-  rect = pygame.Rect(leftTop, widthHeight)
-  if(cell):
-    pygame.draw.rect(screen, "green3", rect)
-  else:
-    pygame.draw.rect(screen, "blue3", rect)
 
 while is_running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       is_running = False
 
-  state = compute_new_state(state)
-  draw_grid(state)
+  rect = pygame.Rect((0, 0), (10, 10))
+  pygame.draw.rect(screen, "green3", rect)
   
   pygame.display.flip()
   clock.tick(10)
--- state.py
+++ state.py
@@ -7,32 +7,3 @@ def get_initial_state(config):
       row.append(cell)
     grid.append(row)
   return grid
-
-def compute_new_state(curr_grid):
-  grid = []
-  for i in range(0, len(curr_grid)):
-    row = []
-    for j in range(0, len(curr_grid[i])):
-      count = get_neighbor_count(curr_grid, i, j)
-      cell = count == 3 or count == 2 and curr_grid[i][j]
-      row.append(cell)
-    grid.append(row)
-  return grid
-
-def get_neighbor_count(grid, i, j):
-  return [
-    is_live(grid, i-1, j),
-    is_live(grid, i+1, j),
-    is_live(grid, i, j-1),
-    is_live(grid, i, j+1),
-    is_live(grid, i-1, j-1),
-    is_live(grid, i-1, j+1),
-    is_live(grid, i+1, j-1),
-    is_live(grid, i+1, j+1)
-  ].count(True)
-
-def is_live(grid, i, j):
-  return is_in_bound(i) and is_in_bound(j) and grid[i][j]
-
-def is_in_bound(index):
-  return index >= 0 and index <= 49
