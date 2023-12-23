0a462b29853499c82fa4b6f01eb334e63dbf12d0
Andy <andy@Andys-MacBook-Pro.local>
2023-12-23 09:14:09 -0500

    game loop

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ with open('config.txt', 'r') as f:
   grid_state = get_initial_state(config)
   
 pygame.init()
+screen = pygame.display.set_mode((500, 500))
+is_running = True
+
+while is_running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      is_running = False
