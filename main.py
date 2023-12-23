33b876451f3217a001800c5c8bc166e6bff6f212
Andy <andy@Andys-MacBook-Pro.local>
2023-12-23 09:58:23 -0500

    draw_cell function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 from state import get_initial_state
 
+def draw_cell(cell, x, y):
+  rect = pygame.Rect((x * 10, y * 10, 10, 10))
+  if(cell):
+    pygame.draw.rect(screen, 'green3', rect)
+  else:
+    pygame.draw.rect(screen, 'blue3', rect)
+
 with open('config.txt', 'r') as f:
   config = f.readlines()
   grid_state = get_initial_state(config)
@@ -15,8 +22,8 @@ while is_running:
     if event.type == pygame.QUIT:
       is_running = False
 
-  rect = pygame.Rect((0, 0, 10, 10))
-  pygame.draw.rect(screen, 'green3', rect)
+  draw_cell(True, 0, 0)
+  draw_cell(False, 1, 0)
   pygame.display.flip()
   clock.tick(10)
 
