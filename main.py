ec061db8f9aebc19a4c726ac1a90f71473b39e78
Andy <andy@Andys-MacBook-Pro.local>
2023-12-23 09:33:26 -0500

    drawing a cell

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ with open('config.txt', 'r') as f:
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 is_running = True
+clock = pygame.time.Clock()
 
 while is_running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       is_running = False
+
+  rect = pygame.Rect((0, 0, 10, 10))
+  pygame.draw.rect(screen, 'green3', rect)
+  pygame.display.flip()
+  clock.tick(10)
+
+pygame.quit()
