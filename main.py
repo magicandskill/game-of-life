ee7c3731f51c8d8384d0bae5522d1579b33b9495
Andy <andy@Andys-MacBook-Pro.local>
2023-12-22 23:32:34 -0500

    initial-config

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import pygame
 from state import get_initial_state
 
 with open('config.txt', 'r') as f:
-  state = get_initial_state(f.readlines())
-  
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-is_running = True
-clock = pygame.time.Clock()
-
-while is_running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      is_running = False
-
-  rect = pygame.Rect((0, 0), (10, 10))
-  pygame.draw.rect(screen, "green3", rect)
-  
-  pygame.display.flip()
-  clock.tick(10)
-
-pygame.quit()
+  config = f.readlines()
+  grid_state = get_initial_state(config)
